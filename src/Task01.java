3dde6a703fb23b684cb89b437a3ff77a7861173e
rinabalakhonova <katia.balahonowa@yandex.ru>
2022-09-13 11:39:36 +0300

    Initial commit

---
+++ src/Task01.java
@@ -0,0 +1,18 @@
+public class Task01 {
+
+    sum {
+
+//        logic
+        int result = 5 + 6;
+    }
+
+    public static void main(String[] args) {
+
+//        output
+        System.out.println(result);
+//
+
+        file.open("text.txt");
+        file.write(result);
+    }
+}
